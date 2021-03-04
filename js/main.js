e73343b7167ee9539f485249fa6241292a9faa4f
Kauan Vitorino <kautec@outlook.com.br>
2021-03-03 21:19:31 -0300

    Jogo da cobrinha DIO

---
+++ js/main.js
@@ -0,0 +1,109 @@
+let canvas =  document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+let snake = [];
+
+var pontos = 0;
+
+snake[0] = {
+    x:8 * box,
+    y:8 * box
+}
+
+let direction = "right";
+
+//floor retira parte flutuando
+let food = {
+    x: Math.floor(Math.random()* 15 + 1) * box,
+    y: Math.floor(Math.random()* 15 + 1) * box
+}
+
+//mostar pontuacao
+let points = document.getElementById("points");
+
+function createBG(){
+    context.fillStyle = 'lightgreen';
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake(){
+    for(i =0 ; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+        
+    }
+}
+
+function createFood(){    
+        context.fillStyle = "red";
+        context.fillRect(food.x, food.y, box, box);  
+}
+
+//pega o evento da tecla 
+document.addEventListener('keydown', update);
+//
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function beginGame(){    
+
+    //Reseta a posição caso chegar no fim do box
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    //
+
+    //Criar uma alert caso bate no corpo
+    for(i = 1; i < snake.length; i++){
+
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert("Game Over  x)");
+        }
+    } 
+    //
+
+    createBG();
+    createSnake();
+    createFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    //Logica de aumentar a snake caso pegue o food
+    if(snakeX != food.x || snakeY != food.y){
+        //retira o ultimo elemento do array
+        snake.pop();
+    }
+    else{
+        //gera o food novamente
+        food.x = Math.floor(Math.random()* 15 + 1) * box;
+        food.y = Math.floor(Math.random()* 15 + 1) * box;
+
+        //pontuacao
+        pontos ++;
+        points.innerHTML = "<b>Pontução: " + pontos +"<b>";
+    }     
+
+    //Cria um novo elemento e define o movimento
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+    //
+}
+
+let game = setInterval(beginGame, 100);
